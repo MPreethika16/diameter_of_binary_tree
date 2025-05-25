001af66befb789611e6b9009570157d066fb1e46
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-05-25 20:28:27 +0530

    Update diameter_of_binary_tree.cpp

--- solutions/diameter_of_binary_tree.cpp
+++ solutions/diameter_of_binary_tree.cpp
@@ -10,17 +10,9 @@ struct TreeNode {
 class Solution {
 public: 
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        height(root, diameter);
-        return diameter;
+        
     }
 
-private:
-    int height(TreeNode* node, int& diameter) {
-        if (!node) return 0;
-        int leftHeight = height(node->left, diameter);
-        int rightHeight = height(node->right, diameter);
-        diameter = std::max(diameter, leftHeight + rightHeight);
-        return std::max(leftHeight, rightHeight) + 1;
-    }
+
+    
 };
