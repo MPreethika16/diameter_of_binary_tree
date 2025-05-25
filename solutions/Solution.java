b141664753f1fa7906419cbd4a5da31c743ba74a
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-05-25 21:55:13 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -1,15 +1,11 @@
 public class Solution {
-    public int diameterOfBinaryTree(TreeNode root) {
-        return 0; // stub - no actual solution
+    public static class TreeNode {
+        int val;
+        TreeNode left, right;
+        public TreeNode(int x) { val = x; }
     }
-} 
 
-// TreeNode class definition
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right; 
-    TreeNode() { val = 0; left = null; right = null; }
-    TreeNode(int x) { val = x; left = null; right = null; }
-    TreeNode(int x, TreeNode left, TreeNode right) { this.val = x; this.left = left; this.right = right; }
+    public int diameterOfBinaryTree(TreeNode root) {
+        return 0;  // stub for now
+    }
 }
