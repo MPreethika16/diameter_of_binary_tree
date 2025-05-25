46bd0cd9f354e5b0d3313efa53e961b51e44b0e4
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-05-25 20:51:09 +0530

    Update diameter_of_binary_tree.py

--- solutions/diameter_of_binary_tree.py
+++ solutions/diameter_of_binary_tree.py
@@ -1,4 +1,3 @@
-def diameterOfBinaryTree(self, root: TreeNode) -> int:
-    # Define height() inside here
-    # Use self.diameter or a helper variable
-    pass
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        pass  # base code / no logic
