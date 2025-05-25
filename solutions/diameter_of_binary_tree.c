39541f26da6b743989df0e7ec2a33bec58802599
MPreethika16 <162781637+MPreethika16@users.noreply.github.com>
2025-05-25 20:22:59 +0530

    Update diameter_of_binary_tree.c

--- solutions/diameter_of_binary_tree.c
+++ solutions/diameter_of_binary_tree.c
@@ -6,24 +6,3 @@ struct TreeNode {
     struct TreeNode *right; 
 };
 
-static int height(struct TreeNode* node, int* diameter) {
-    if (!node) return 0;
-    int left = height(node->left, diameter);
-    int right = height(node->right, diameter);
-    if (*diameter < left + right) *diameter = left + right;
-    return (left > right ? left : right) + 1;
-}
-
-int diameterOfBinaryTree(struct TreeNode* root) {
-    int diameter = 0;
-    height(root, &diameter);
-    return diameter;
-}
-
-struct TreeNode* newNode(int val) {
-    struct TreeNode* node = malloc(sizeof(struct TreeNode));
-    node->val = val;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
-}
